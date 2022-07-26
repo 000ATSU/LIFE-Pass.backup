de67e3f76a9afad00da433f42ca6927c2dd6547e
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-07-26 15:43:05 +0000

    add ranking

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,13 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.page(params[:page]).per(1)
-    @user = User.all
-    @rank = @user.sort_by{|favorites| favorites }
+    @posts = Post.page(params[:page]).per(6)
+    @users = User.all
+    @rank = @users.map do |user|
+      user.favorites.count
+    end
+    # @rank = Favorite.group(:post_id).order('count(post_id) desc').limit(5)
+    # @rank.each {|v| v.user.user_name}
+    # binding.pry
   end
 
   def about
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,6 +3,8 @@ class Public::UsersController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @posts = @user.posts
+    @get_favorite = Favorite.where(post_id: @posts.ids)
+    binding.pry
   end
 
   def show
